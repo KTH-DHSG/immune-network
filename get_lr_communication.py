e1bc1273d2f7de1bee78d2966dfce12c7f6d7bcd
ptajvar <ptajvar@gmail.com>
2022-05-25 15:22:15 +0200

    Adapted the get_lr_communication.py and plot_communication_matrices.py to the new covid dataset.

--- get_lr_communication.py
+++ get_lr_communication.py
@@ -1,10 +1,12 @@
+import os
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scanpy
 from copy import copy
 import pickle
-
+import os
 
 def all_genes_present(check_list, gene_library):
     return all([gene in gene_library for gene in check_list])
@@ -12,49 +14,65 @@ def all_genes_present(check_list, gene_library):
 
 def get_cell_type(dataset, cell_type):
     '''returns only the cells of cell_type from dataset'''
-    mask = dataset.obs['ct_cov'] == cell_type
+    mask = dataset.obs['cell_type'] == cell_type
     return dataset[mask]
 
 
 def separate_by_celltype(dataset):
     '''returns cell types present in dataset and a dictionary mapping each cell_type name to the dataset of cells in
     dataset with that type'''
-    cell_types = list(set([d.obs['ct_cov'][0] for d in dataset]))
+    cell_types = list(set([d.obs['cell_type'][0] for d in dataset]))
+    cell_types.sort()
     cells_dict = dict()
     for ct in cell_types:
         specific_cells = get_cell_type(dataset, ct)
         cells_dict[ct] = copy(specific_cells)
     return cell_types, cells_dict
 
+spec_type = 'mean' # mean or sum
 
 ligand_recep_db = pd.read_csv('../NATMI/lrdbs/lrc2p.csv')
-samples_path = '../longitudinal_samples_healthy/'
-sample_name = 'IGTB1290_batch0.h5ad'
-sample = scanpy.read_h5ad(samples_path+sample_name)
-gene_library = sample.to_df().columns
-cell_types, cells_dict = separate_by_celltype(sample)
-cell_types_n = len(cell_types)
-pairs = []
-communication_spec_matrices = np.zeros((cell_types_n, cell_types_n, 0))
-for id in ligand_recep_db.index:
-    ligand = ligand_recep_db.iloc[id]['Ligand gene symbol']
-    receptor = ligand_recep_db.iloc[id]['Receptor gene symbol']
-    if not all_genes_present([ligand, receptor], gene_library):
-        continue
-    ligand_expression = np.zeros((cell_types_n, 1))
-    receptor_expression = np.zeros((cell_types_n, 1))
-    for id in range(len(cell_types)):
-        ligand_expression[id] = cells_dict[cell_types[id]].to_df()[ligand].mean()
-        receptor_expression[id] = cells_dict[cell_types[id]].to_df()[receptor].mean()
-    communication_sum = (ligand_expression@receptor_expression.T).reshape(cell_types_n, cell_types_n, 1)
-    communication_spec = communication_sum / np.sum(communication_sum)
-    communication_spec_matrices = np.concatenate((communication_spec_matrices, communication_spec), axis=2)
-    pairs.append([ligand, receptor])
-'''saving communication matrices'''
-communication_dict = {}
-communication_dict['matrices'] = communication_spec_matrices
-communication_dict['pairs'] = pairs
-communication_dict['cell_types'] = cell_types
+samples_path = '../covid_separated/'
+sample_names = [s for s in os.listdir(samples_path) if 'h5ad' in s]
 
-with open('main/specificiyty_matrices.pickle', 'wb') as handle:
-    pickle.dump(communication_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+for sample_name in sample_names:
+    sample = scanpy.read_h5ad(samples_path+sample_name)
+    gene_library = sample.to_df().columns
+    cell_types, cells_dict = separate_by_celltype(sample)
+    mean_expression_dict = dict()
+    sum_expression_dict = dict()
+    for c in cells_dict.keys():
+        mean_expression_dict[c] = cells_dict[c].to_df().mean()
+        sum_expression_dict[c] = cells_dict[c].to_df().sum()
+    cell_types_n = len(cell_types)
+    pairs = []
+    communication_spec_matrices = np.zeros((cell_types_n, cell_types_n, 0))
+    for id in ligand_recep_db.index:
+        ligand = ligand_recep_db.iloc[id]['Ligand gene symbol']
+        receptor = ligand_recep_db.iloc[id]['Receptor gene symbol']
+        if not all_genes_present([ligand, receptor], gene_library):
+            continue
+        ligand_expression = np.zeros((cell_types_n, 1))
+        receptor_expression = np.zeros((cell_types_n, 1))
+        for id in range(len(cell_types)):
+            if spec_type == 'mean':
+                ligand_expression[id] = mean_expression_dict[cell_types[id]][ligand]
+                receptor_expression[id] = mean_expression_dict[cell_types[id]][receptor]
+            elif spec_type == 'sum':
+                ligand_expression[id] = sum_expression_dict[cell_types[id]][ligand]
+                receptor_expression[id] = sum_expression_dict[cell_types[id]][receptor]
+            else:
+                print('unkown spec_type ' + spec_type)
+
+        communication_sum = (ligand_expression@receptor_expression.T).reshape(cell_types_n, cell_types_n, 1)
+        communication_spec = communication_sum / np.sum(communication_sum)
+        communication_spec_matrices = np.concatenate((communication_spec_matrices, communication_spec), axis=2)
+        pairs.append([ligand, receptor])
+    '''saving communication matrices'''
+    communication_dict = {}
+    communication_dict['matrices'] = communication_spec_matrices
+    communication_dict['pairs'] = pairs
+    communication_dict['cell_types'] = cell_types
+
+    with open('covid_data/' + sample_name + 'specificiyty_' + spec_type + '_matrices.pickle', 'wb') as handle:
+        pickle.dump(communication_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
--- plot_communication_matrices.py
+++ plot_communication_matrices.py
@@ -1,33 +1,39 @@
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-with open('specificiyty_matrices.pickle', 'rb') as handle:
-    specificity_dict = pickle.load(handle)
+folder = 'covid_data/'
+files = [f for f in os.listdir(folder) if 'pickle' in f]
 
-matrices = specificity_dict['matrices']
-genes = specificity_dict['matrices']
-cell_types = specificity_dict['cell_types']
+for f in files:
+    with open(folder+f, 'rb') as handle:
+        specificity_dict = pickle.load(handle)
 
-sum_of_specificity = np.nansum(matrices, axis=2)
-normalized_sum = sum_of_specificity / np.max(sum_of_specificity)
+    matrices = specificity_dict['matrices']
+    genes = specificity_dict['matrices']
+    cell_types = specificity_dict['cell_types']
 
-fig, ax = plt.subplots()
-im = ax.imshow(sum_of_specificity)
+    sum_of_specificity = np.nansum(matrices, axis=2)
+    normalized_sum = sum_of_specificity / np.max(sum_of_specificity)
 
-ax.set_xticks(np.arange(len(cell_types)), labels=cell_types)
-ax.set_yticks(np.arange(len(cell_types)), labels=cell_types)
+    fig, ax = plt.subplots()
+    im = ax.imshow(sum_of_specificity)
 
-# Rotate the tick labels and set their alignment.
-plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-         rotation_mode="anchor")
+    ax.set_xticks(np.arange(len(cell_types)), labels=cell_types)
+    ax.set_yticks(np.arange(len(cell_types)), labels=cell_types)
 
-# Loop over data dimensions and create text annotations.
-for i in range(len(cell_types)):
-    for j in range(len(cell_types)):
-        text = ax.text(j, i, "{:10.1f}".format(sum_of_specificity[i, j]),
-                       ha="center", va="center", color="w")
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
 
-ax.set_title("Sum of specificity in ligand-receptor communication")
-# fig.tight_layout()
-plt.show()
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(cell_types)):
+        for j in range(len(cell_types)):
+            text = ax.text(j, i, "{:10.1f}".format(sum_of_specificity[i, j]),
+                           ha="center", va="center", color="w")
+
+    ax.set_title("Sum of specificity in ligand-receptor communication")
+    fig.set_size_inches(18.5, 10.5)
+    plt.savefig(folder+f.replace('pickle', 'png'))
+    plt.close()
