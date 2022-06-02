11e26ed2a01d2466d020d8c7b8f9ce0228073880
ptajvar <ptajvar@gmail.com>
2022-06-02 16:31:34 +0200

    sparse_pca_gene.py can now be used to plot PCA of samples based on both mean gene expression and total gene expressions.

--- sparse_pca_gene.py
+++ sparse_pca_gene.py
@@ -4,17 +4,16 @@ import matplotlib.pyplot as plt
 import pickle
 import os
 
-spec_type = 'mean' # mean or sum
-
 folder = 'covid_data_summary/'
 files = [f for f in os.listdir(folder) if 'pickle' in f]
 
-spec_type = 'mean'
+expression_type = 'sum' # mean or sum
 days = []
 sos_vectors = None
 patient = ''
 patients = []
 tot_expressions = []
+mean_expressions = []
 for f in files:
     if 'patient' + patient not in f:
         continue
@@ -23,24 +22,35 @@ for f in files:
     with open(folder+f, 'rb') as handle:
         summary_dict = pickle.load(handle)
     tot_expression = None
+    tot_count = 0
     for ct in summary_dict.keys():
         if tot_expression is None:
-            tot_expression = np.zeros((summary_dict[ct]['gene_expression'].shape))
+            tot_expression = np.zeros(summary_dict[ct]['gene_expression'].shape)
+            mean_expression = np.zeros(summary_dict[ct]['gene_expression'].shape)
+
         tot_expression += summary_dict[ct]['gene_expression'].to_numpy()*summary_dict[ct]['cell_count']
+        tot_count += summary_dict[ct]['cell_count']
     tot_expressions.append(tot_expression)
+    mean_expressions.append(tot_expression/tot_count)
 
 tot_expressions = np.array(tot_expressions)
+mean_expressions = np.array(mean_expressions)
+
+if expression_type == 'mean':
+    data = mean_expressions
+elif expression_type == 'sum':
+    data = tot_expressions
 
 n_components = 3
 transformer = SparsePCA(n_components=n_components, random_state=0)
 # transformer = PCA(n_components=n_components, random_state=0)
-transformer.fit(tot_expressions)
-tot_expressions_transformed = transformer.transform(tot_expressions)
-tot_expressions_recovered = np.dot(tot_expressions_transformed, transformer.components_) + transformer.mean_
+transformer.fit(data)
+data_transformed = transformer.transform(data)
+data_recovered = np.dot(data_transformed, transformer.components_) + transformer.mean_
 
-print('mean error naive: ', np.linalg.norm(tot_expressions-transformer.mean_))
+print('mean error naive: ', np.linalg.norm(data-transformer.mean_))
 
-print('mean error PCA: ', np.linalg.norm(tot_expressions-tot_expressions_recovered))
+print('mean error PCA: ', np.linalg.norm(data-data_recovered))
 
 patiens_set = sorted(set(patients))
 
@@ -48,13 +58,27 @@ fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 plot_list = []
 plotted_set = sorted([])
+
+# Healthy persons
+mask = np.zeros((len(patients), ))
 for p in patiens_set:
-    if p not in ['A', 'K', 'L', 'M']:
+    if p not in ['K', 'L', 'M']:
         continue
-    plotted_set.append(p)
-    mask = np.array(patients) == p
-    plot_name = ax.scatter(tot_expressions_transformed[mask, 0], tot_expressions_transformed[mask, 1],
-                           tot_expressions_transformed[mask, 2], )
-    plot_list.append(plot_name)
-ax.legend(plot_list, ['Patient ' + p for p in plotted_set])
+    mask = np.logical_or(mask, np.array(patients) == p)
+
+healthy_plot_name = ax.scatter(data_transformed[mask, 0], data_transformed[mask, 1],
+                       data_transformed[mask, 2])
+
+# Affected persons
+mask = np.zeros((len(patients), ))
+for p in patiens_set:
+    if p in ['K', 'L', 'M']:
+        continue
+    mask = np.logical_or(mask, np.array(patients) == p)
+
+
+affected_plot_name = ax.scatter(data_transformed[mask, 0], data_transformed[mask, 1],
+                           data_transformed[mask, 2])
+ax.legend([healthy_plot_name, affected_plot_name], ['Healthy persons', 'Affected persons'])
+plt.savefig('gene_expression_pca.png')
 plt.show()
